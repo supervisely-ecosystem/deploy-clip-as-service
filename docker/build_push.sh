980e15d947b6493aedf06d8233c4428f1a49d46a
GoldenAnpu <seryios.sych@gmail.com>
2025-07-22 13:09:18 +0000

    Update Docker image version to 0.1.2 and refactor entrypoint in config.json; add run.py for improved logging

--- docker/build_push.sh
+++ docker/build_push.sh
@@ -1,4 +1,4 @@
 cp ../dev_requirements.txt . && \
-docker build --no-cache -t supervisely/deploy-cas:0.1.1 . && \
+docker build --no-cache -t supervisely/deploy-cas:0.1.2 . && \
 rm dev_requirements.txt && \
-docker push supervisely/deploy-cas:0.1.1
+docker push supervisely/deploy-cas:0.1.2
---
+++ run.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+if log_level in ["DEBUG", "INFO", "WARNING"]:
+    os.environ["JINA_LOG_LEVEL"] = log_level
+
+proc = subprocess.Popen(
+    ["python", "-m", "clip_server", "clip.yml"],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    text=True,
+)
+
+for line in proc.stdout:
+    s = line.strip()
+
+    # Remove empty lines
+    if not s:
+        continue
+    
+    log_levels = ["INFO", "DEBUG", "WARNING", "ERROR"]
+
+    # Check if the message is not empty after the log level
+    if any(s.startswith(level) or level in s for level in log_levels):
+        # Skip the line if it consists only of the log level and date
+        if s in [f"{level}{s[-19:]}" for level in log_levels]:
+            continue
+
+    print(line, end="")
